9261f6cf8dde741ba90a91f12b41d14283db387c
Brandon Fish <brandon.j.fish@gmail.com>
2019-02-17 18:03:10 -0600

    Update API example for latest C API changes

--- wasmer-c-api-example.c
+++ wasmer-c-api-example.c
@@ -5,18 +5,37 @@
 
 static print_str_called = false;
 
-void print_str(int32_t ptr, int32_t len, wasmer_instance_context_t *ctx)
+void print_str(wasmer_instance_context_t *ctx, int32_t ptr, int32_t len)
 {
-    printf("In print_str\n");
     print_str_called = true;
+    wasmer_memory_t *memory = wasmer_instance_context_memory(ctx, 0);
+    uint32_t mem_len = wasmer_memory_length(memory);
+    uint8_t *mem_bytes = wasmer_memory_data(memory);
+    printf("%.*s", mem_len, mem_bytes);
 }
 
 int main()
 {
-    wasmer_import_object_t *import_object = wasmer_import_object_new();
+
     wasmer_value_tag params_sig[] = {WASM_I32, WASM_I32};
     wasmer_value_tag returns_sig[] = {};
-    wasmer_imports_set_import_func(import_object, "env", "print_str", print_str, params_sig, 2, returns_sig, 0);
+    wasmer_func_t *func = wasmer_func_new(print_str, params_sig, 2, returns_sig, 0);
+    wasmer_import_t import;
+
+    char *module_name = "env";
+    wasmer_byte_array module_name_bytes;
+    module_name_bytes.bytes = module_name;
+    module_name_bytes.bytes_len = strlen(module_name);
+    char *import_name = "print_str";
+    wasmer_byte_array import_name_bytes;
+    import_name_bytes.bytes = import_name;
+    import_name_bytes.bytes_len = strlen(import_name);
+
+    import.module_name = module_name_bytes;
+    import.import_name = import_name_bytes;
+    import.tag = WASM_FUNCTION;
+    import.value.func = func;
+    wasmer_import_t imports[] = {import};
 
     // Read the wasm file bytes
     FILE *file = fopen("wasm-sample-app/target/wasm32-unknown-unknown/release/wasm_sample_app.wasm", "r");
@@ -28,22 +47,28 @@ int main()
     fclose(file);
 
     wasmer_instance_t *instance = NULL;
-    wasmer_compile_result_t compile_result = wasmer_instantiate(&instance, bytes, len, import_object);
+    wasmer_result_t compile_result = wasmer_instantiate(&instance, bytes, len, imports, 1);
     printf("Compile result:  %d\n", compile_result);
-    assert(compile_result == WASMER_COMPILE_OK);
+    if (compile_result != WASMER_OK)
+    {
+        int error_len = wasmer_last_error_length();
+        printf("Error len: `%d`\n", error_len);
+        char *error_str = malloc(error_len);
+        wasmer_last_error_message(error_str, error_len);
+        printf("Error str: `%s`\n", error_str);
+    }
+    assert(compile_result == WASMER_OK);
 
     wasmer_value_t params[] = {};
     wasmer_value_t results[] = {};
-    wasmer_call_result_t call_result = wasmer_instance_call(instance, "hello_wasm", params, 0, results, 0);
+    wasmer_result_t call_result = wasmer_instance_call(instance, "hello_wasm", params, 0, results, 0);
     printf("Call result:  %d\n", call_result);
-    assert(call_result == WASMER_CALL_OK);
+    assert(call_result == WASMER_OK);
 
     assert(print_str_called);
 
-    // TODO review import object ownership, instantiate moves it
-    printf("Destroy instance\n");
-    //wasmer_instance_destroy(instance); // TODO error here
-    printf("Destroy import object\n");
-    //wasmer_import_object_destroy(import_object); // TODO error here
+    // printf("Destroy instance\n");
+    // wasmer_instance_destroy(instance);
+
     return 0;
 }
